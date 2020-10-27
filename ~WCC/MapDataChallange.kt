bf8d58e800edeb3d2414a544f829deb16b928ba0
jesstoselli <jfamedeiros@gmail.com>
2020-10-27 12:19:09 -0300

    WWC - Challenge completed with lambda functions

--- ~WCC/MapDataChallange.kt
+++ ~WCC/MapDataChallange.kt
@@ -1,10 +1,10 @@
 fun main() {
 
     val rawData = mapOf(
-        "users1.csv" to listOf(32, 45, 17, -1, 34),
-        "users2.csv" to listOf(19, -1, 67, 22),
-        "users3.csv" to listOf(15, -124, 0, 12),
-        "users4.csv" to listOf(56, 32, 18, 44)
+            "users1.csv" to listOf(32, 45, 17, -1, 34),
+            "users2.csv" to listOf(19, -1, 67, 22),
+            "users3.csv" to listOf(15, -124, 0, 12),
+            "users4.csv" to listOf(56, 32, 18, 44)
     )
 
     // Question 1
@@ -23,7 +23,8 @@ fun main() {
 
     averageAges(rawData)
 
-    // Functional programming solution
+
+    // Functional Programming solution
     fun average(map: Map<String, List<Int>>): Double {
 
         val average = map
@@ -35,7 +36,6 @@ fun main() {
     }
 
     println(average(rawData))
-
 
     // Question 2
     fun entriesWithFaultyValues(map: Map<String, List<Int>>) {
@@ -50,8 +50,18 @@ fun main() {
 
     entriesWithFaultyValues(rawData)
 
+    // Functional Programming solution
+    fun faultyValues(map: Map<String, List<Int>>) {
 
-    // Functional programming solution
+        val faultyValuesKeys = map
+                .filter { it.value.any { it <= 0 } }
+                .map { it.key }
+
+        println(faultyValuesKeys)
+    }
+
+    faultyValues(rawData)
+
 
     // Question 3
     fun quantityOfFaultyValues(map: Map<String, List<Int>>) {
@@ -69,4 +79,17 @@ fun main() {
     }
 
     quantityOfFaultyValues(rawData)
+
+    // Functional Programming solution
+    fun qtyOfFaultyValues(map: Map<String, List<Int>>) {
+
+        val qtyOfFaultyValues = map
+                .flatMap { it.value }
+                .filter { it <= 0 }
+                .count()
+
+        println(qtyOfFaultyValues)
+    }
+
+    qtyOfFaultyValues(rawData)
 }
