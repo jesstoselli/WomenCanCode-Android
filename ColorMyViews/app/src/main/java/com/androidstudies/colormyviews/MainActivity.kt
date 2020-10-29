dd414469c3e081fc4aca937bbf87111d3f8a1f3b
jesstoselli <jfamedeiros@gmail.com>
2020-10-29 20:06:48 -0300

    ColorMyViews - Color buttons added.

--- ColorMyViews/app/src/main/java/com/androidstudies/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/androidstudies/colormyviews/MainActivity.kt
@@ -2,10 +2,36 @@ package com.androidstudies.colormyviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.graphics.drawable.toDrawable
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+        var currentColor = R.color.grey
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setColorButtonsAction()
     }
+
+        private fun setColorButtonsAction() {
+
+            btn_red.setOnClickListener {
+                currentColor = R.color.red
+            }
+
+            btn_yellow.setOnClickListener {
+                currentColor = R.color.yellow
+            }
+
+            btn_green.setOnClickListener {
+                currentColor = R.color.green
+            }
+
+        }
+
 }
