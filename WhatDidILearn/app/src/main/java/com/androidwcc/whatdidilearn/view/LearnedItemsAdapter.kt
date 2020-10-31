32af5b3362d32bbb791982c1715ece33bc928068
jesstoselli <jfamedeiros@gmail.com>
2020-10-31 14:39:50 -0300

    WhatDidILearn - creating adapter view holder class.

--- WhatDidILearn/app/src/main/java/com/androidwcc/whatdidilearn/view/LearnedItemsAdapter.kt
+++ WhatDidILearn/app/src/main/java/com/androidwcc/whatdidilearn/view/LearnedItemsAdapter.kt
@@ -2,13 +2,25 @@ package com.androidwcc.whatdidilearn.view
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.androidwcc.whatdidilearn.R
 import com.androidwcc.whatdidilearn.entities.ItemLearned
 
 class LearnedItemsAdapter: RecyclerView.Adapter<LearnedItemsAdapter.LearnedItemViewHolder>() {
     var data = listOf<ItemLearned>()
 
     inner class LearnedItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val titleItem: TextView = itemView.findViewById(
+            R.id.textView_title)
+        val descriptionItem: TextView = itemView.findViewById(R.id.textView_description)
+        val levelItem: View = itemView.findViewById(R.id.linearLayout_itemLevel)
+
+        fun bind(title: String, description: String, color: Int) {
+            titleItem.text = title
+            descriptionItem.text = description
+            levelItem.setBackgroundColor(color)
+        }
 
     }
 
