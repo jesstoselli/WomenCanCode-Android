e6841913b61db133e8aef084070607cf9456b00d
jesstoselli <jfamedeiros@gmail.com>
2020-10-31 11:53:46 -0300

    WhatDidILearn - adding Recycle View.

---
+++ WhatDidILearn/app/src/main/java/com/androidwcc/whatdidilearn/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.androidwcc.whatdidilearn.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.androidwcc.whatdidilearn.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
