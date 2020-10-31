42a544ab968b708cb0c98964d38462f22d8cd6fd
jesstoselli <jfamedeiros@gmail.com>
2020-10-31 11:19:24 -0300

    WhatDidILearn - creating class ItemLearned and enum UnderstandingLevel.

---
+++ WhatDidILearn/app/src/main/java/com/androidwcc/whatdidilearn/entities/ItemLearned.kt
@@ -0,0 +1,7 @@
+package com.androidwcc.whatdidilearn.entities
+
+data class ItemLearned (
+    val title: String,
+    val description: String,
+    val understandingLevel: UnderstandingLevel,
+)
---
+++ WhatDidILearn/app/src/main/java/com/androidwcc/whatdidilearn/entities/UnderstandingLevel.kt
@@ -0,0 +1,7 @@
+package com.androidwcc.whatdidilearn.entities
+
+enum class UnderstandingLevel {
+    HIGH,
+    MEDIUM,
+    LOW
+}
