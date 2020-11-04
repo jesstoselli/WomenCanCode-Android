f1df0be897f7d774c9f06780a4808e479a9bcf4e
jesstoselli <jfamedeiros@gmail.com>
2020-11-04 20:31:42 -0300

    WhatDidILearn - Adding populate database function.

--- WhatDidILearn/app/src/main/java/com/androidwcc/whatdidilearn/data/DatabaseItems.kt
+++ WhatDidILearn/app/src/main/java/com/androidwcc/whatdidilearn/data/DatabaseItems.kt
@@ -5,8 +5,11 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.androidwcc.whatdidilearn.entities.LearnedItem
 import com.androidwcc.whatdidilearn.entities.UnderstandingLevel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = [LearnedItem::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
@@ -66,8 +69,6 @@ abstract class DatabaseItems: RoomDatabase() {
                 "View Binding",
                 "View Binding é um recurso que facilita a programação de códigos que interagem com views.\n",
                 UnderstandingLevel.LOW
-
-
             )
 
             return listOf<LearnedItem>(
@@ -77,6 +78,54 @@ abstract class DatabaseItems: RoomDatabase() {
                 itemFour,
                 itemFive
             )
+        }
+    }
+
+    private class DatabaseCallBack(
+        private val scope: CoroutineScope
+    ): RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch { populateDatabase(database.learnedItemDao()) }
+            }
+        }
+        suspend fun populateDatabase(dao: LearnedItemDao) {
+            val itemOne = LearnedItem(
+                "Kotlin - Null safety",
+                "O sistema de tipos de Kotlin visa eliminar o perigo de referências nulas do código.",
+                UnderstandingLevel.HIGH
+            )
+
+            val itemTwo = LearnedItem(
+                "Layout editor",
+                "O Design Editor exibe o layout em vários dispositivos e versões do Android.É possível criar e editar um layout usando apenas componentes visuais.",
+                UnderstandingLevel.MEDIUM
+            )
+
+            val itemThree = LearnedItem(
+                "Git",
+                "É um sistema de controle de versão distribuído. Com ele é possível rastrear mudanças no código-fonte durante o desenvolvimento de software.",
+                UnderstandingLevel.MEDIUM
+            )
+
+            val itemFour = LearnedItem(
+                "GroupView",
+                "É uma view especial que pode conter outras views (chamadas de filhos).É a classe base para layouts e contêineres de views.",
+                UnderstandingLevel.HIGH
+            )
+
+            val itemFive = LearnedItem(
+                "View Binding",
+                "View Binding é um recurso que facilita a programação de códigos que interagem com views.\n",
+                UnderstandingLevel.LOW
+            )
+
+            dao.insert(itemOne)
+            dao.insert(itemTwo)
+            dao.insert(itemThree)
+            dao.insert(itemFour)
+            dao.insert(itemFive)
         }
     }
 }
